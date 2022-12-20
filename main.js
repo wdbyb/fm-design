320b37841f0bf9354900ba45e3a6622b424b640d
Вини <yvinivitin@gmail.com>
2022-12-20 12:30:03 +0200

    desktop all pages

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 import './styles/main.scss';
+
+const mapCanada = L.map('map-canada', {
+  center: [43.64414050035226, -79.39457158840445],
+  zoom: 16,
+});
+
+L.tileLayer('http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}', {
+  maxZoom: 30,
+  subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+}).addTo(mapCanada);
+
+const mapAustralia = L.map('map-australia', {
+  center: [-30.32388305775135, 149.7855395538651],
+  zoom: 16,
+});
+
+L.tileLayer('http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}', {
+  maxZoom: 30,
+  subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+}).addTo(mapAustralia);
+
+const mapUK = L.map('map-uk', {
+  center: [51.73270879965942, -3.8619428402317872],
+  zoom: 16,
+});
+
+L.tileLayer('http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}', {
+  maxZoom: 30,
+  subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+}).addTo(mapUK);
