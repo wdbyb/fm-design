8c7f9785dae4f05f26965a05a0b3b02350a1fb55
Вини <yvinivitin@gmail.com>
2022-12-21 15:55:42 +0200

    sections background

--- main.js
+++ main.js
@@ -7,32 +7,32 @@ menuBtn.addEventListener('click', () => {
   document.body.classList.toggle('overflow-hidden');
 });
 
-// const mapCanada = L.map('map-canada', {
-//   center: [43.64414050035226, -79.39457158840445],
-//   zoom: 16,
-// });
+const mapCanada = L.map('map-canada', {
+  center: [43.64414050035226, -79.39457158840445],
+  zoom: 16,
+});
 
-// L.tileLayer('http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}', {
-//   maxZoom: 30,
-//   subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
-// }).addTo(mapCanada);
+L.tileLayer('http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}', {
+  maxZoom: 30,
+  subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+}).addTo(mapCanada);
 
-// const mapAustralia = L.map('map-australia', {
-//   center: [-30.32388305775135, 149.7855395538651],
-//   zoom: 16,
-// });
+const mapAustralia = L.map('map-australia', {
+  center: [-30.32388305775135, 149.7855395538651],
+  zoom: 16,
+});
 
-// L.tileLayer('http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}', {
-//   maxZoom: 30,
-//   subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
-// }).addTo(mapAustralia);
+L.tileLayer('http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}', {
+  maxZoom: 30,
+  subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+}).addTo(mapAustralia);
 
-// const mapUK = L.map('map-uk', {
-//   center: [51.73270879965942, -3.8619428402317872],
-//   zoom: 16,
-// });
+const mapUK = L.map('map-uk', {
+  center: [51.73270879965942, -3.8619428402317872],
+  zoom: 16,
+});
 
-// L.tileLayer('http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}', {
-//   maxZoom: 30,
-//   subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
-// }).addTo(mapUK);
+L.tileLayer('http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}', {
+  maxZoom: 30,
+  subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+}).addTo(mapUK);
