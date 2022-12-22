54aefbae81fe576fddc62354f9f0bd43f4be7f03
Вини <yvinivitin@gmail.com>
2022-12-23 01:41:13 +0200

    gsap settings

--- main.js
+++ main.js
@@ -39,3 +39,103 @@ if (document.querySelector('.location')) {
     createMap(maps[i]);
   }
 }
+
+gsap.registerPlugin(ScrollTrigger);
+
+const heroInfo = document.querySelector('.hero__info');
+const nav = document.querySelector('.nav');
+const links = document.querySelectorAll('.nav__link');
+const logo = document.querySelector('.logo');
+
+// gsap.to('.nav__link', {
+//   x: -50,
+//   duration: 1,
+//   stagger: 0.5,
+// });
+
+// gsap.from(logo, {
+//   x: -100,
+//   opacity: 0,
+//   duration: 1,
+// });
+
+const tl = gsap
+  .timeline()
+  .from(links, {
+    x: 100,
+    opacity: 0,
+    duration: 1,
+    stagger: 0.5,
+  })
+  .from(
+    logo,
+    {
+      x: -100,
+      opacity: 0,
+      duration: 1,
+    },
+    '<'
+  )
+  .from(
+    heroInfo,
+    {
+      y: 100,
+      opacity: 0,
+      duration: 2,
+    },
+    '-=3'
+  )
+  .from(
+    '.hero__image',
+    {
+      x: 150,
+      opacity: 0,
+      duration: 1.5,
+    },
+    '-=3'
+  );
+
+const moo = gsap.fromTo(
+  '.our-designs .our-designs__item',
+  {
+    opacity: 0,
+    y: 250,
+  },
+  {
+    opacity: 1,
+    y: 0,
+    duration: 1,
+    stagger: 0.5,
+    scrollTrigger: {
+      trigger: '.hero',
+      immediateRender: false,
+      start: 'bottom 60%',
+      end: 'bottom center',
+      // markers: true,
+      toggleActions: 'play none none reverse',
+    },
+  }
+);
+
+moo.play();
+
+// const containers = gsap.utils.toArray('.grid-cols-2');
+
+// containers.forEach(function (container) {
+//   gsap.fromTo(
+//     container.querySelectorAll('.box'),
+//     { y: -250 },
+//     {
+//       y: 0,
+//       stagger: 0.3,
+//       scrollTrigger: {
+//         trigger: container,
+//         scrub: true,
+//         start: 'top bottom',
+//         end: 'top 20%',
+//         immediateRender: false,
+//         markers: true,
+//       },
+//     }
+//   );
+// });
